83250e61374de1da38ae98cbe0081af898f5ad60
Almaz Ibragimov <almazrafi@gmail.com>
2019-11-01 14:32:35 +0300

    Fixed color styles encoder

--- Sources/Fugen/Encoders/ColorStyles/DefaultColorStylesEncoder.swift
+++ Sources/Fugen/Encoders/ColorStyles/DefaultColorStylesEncoder.swift
@@ -22,6 +22,6 @@ final class DefaultColorStylesEncoder: ColorStylesEncoder {
     }
 
     func encodeColorStyles(_ colorStyles: [ColorStyle]) -> [String: Any] {
-        return ["styles": colorStyles.map(encodeColorStyle)]
+        return ["colorStyles": colorStyles.map(encodeColorStyle)]
     }
 }
