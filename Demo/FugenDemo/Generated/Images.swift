b3933d6ed3b23001d5a0c41d814493c9ca7aa18f
Almaz Ibragimov <almazrafi@gmail.com>
2019-12-01 06:15:17 +0300

    Images template & demo

---
+++ Demo/FugenDemo/Generated/Images.swift
@@ -0,0 +1,36 @@
+// swiftlint:disable all
+// Generated using Fugen - https://github.com/almazrafi/Fugen
+#if canImport(UIKit)
+import UIKit
+#else
+import AppKit
+#endif
+
+public enum Images {
+
+    // MARK: - Enumeration Cases
+
+    /// Menu
+    public static let menu = UIImage(named: "Menu")!
+
+    /// User
+    public static let user = UIImage(named: "User")!
+
+    /// Settings
+    public static let settings = UIImage(named: "Settings")!
+
+    /// Notification
+    public static let notification = UIImage(named: "Notification")!
+
+    /// Notification Alert
+    public static let notificationAlert = UIImage(named: "NotificationAlert")!
+
+    /// Document
+    public static let document = UIImage(named: "Document")!
+
+    /// Edit
+    public static let edit = UIImage(named: "Edit")!
+
+    /// Search
+    public static let search = UIImage(named: "Search")!
+}
