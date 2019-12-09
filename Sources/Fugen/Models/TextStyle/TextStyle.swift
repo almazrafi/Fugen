03c6504901a27069897f82529e69d39416cce722
Almaz Ibragimov <almazrafi@gmail.com>
2019-12-09 23:46:48 +0300

    Fixed duplication of identical text styles with different colors

--- Sources/Fugen/Models/TextStyle/TextStyle.swift
+++ Sources/Fugen/Models/TextStyle/TextStyle.swift
@@ -4,16 +4,6 @@ struct TextStyle: Hashable {
 
     // MARK: - Instance Properties
 
-    let id: String
-    let name: String
-    let description: String?
-    let font: Font
-    let colorStyle: String?
-    let color: Color
-    let strikethrough: Bool
-    let underline: Bool
-    let paragraphSpacing: Double?
-    let paragraphIndent: Double?
-    let lineHeight: Double?
-    let letterSpacing: Double?
+    let info: TextStyleNodeInfo
+    let colorInfo: TextStyleColorInfo
 }
