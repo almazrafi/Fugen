775faa3d8ea94607767598b58c8738124d5e1175
Almaz Ibragimov <almazrafi@gmail.com>
2020-01-31 21:08:23 +0300

    DictionaryCoder as dependency

--- Sources/FugenTools/DictionaryCoder/Decoder/Strategies/DictionaryKeyDecodingStrategy.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public enum DictionaryKeyDecodingStrategy {
-
-    // MARK: - Enumeration Cases
-
-    case useDefaultKeys
-    case custom((_ codingPath: [CodingKey]) -> CodingKey)
-}
--- Sources/FugenTools/DictionaryCoder/Encoder/Strategies/DictionaryNonConformingFloatEncodingStrategy.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public enum DictionaryNonConformingFloatEncodingStrategy {
-
-    // MARK: - Enumeration Cases
-
-    case `throw`
-    case convertToString(positiveInfinity: String, negativeInfinity: String, nan: String)
-}
